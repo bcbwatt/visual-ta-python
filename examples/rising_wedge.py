bb64fc81d6b5f93f43c59de077464b739826c2bf
krakkus <45657312+krakkus@users.noreply.github.com>
2020-06-07 13:58:29 +0200

    .

--- examples/rising_wedge.py
+++ examples/rising_wedge.py
@@ -6,9 +6,6 @@ from pythonta import drawing as dt
 
 
 def main():
-    # Something to choose from
-    # ^GSPC TSLA BTC-INR ^AEX ^AMX MSFT IWO BTC-USD
-
     # Download a pandas dataframe containing stock data
     df = yf.download('^GSPC',
                      start='2019-06-01',
@@ -19,16 +16,8 @@ def main():
     graph = dt.Graph(width=1280, height=1280)
     graph.clear()
 
-    # Split it into two graphs with a ratio of 5 to 1
-    candle_graph, volume_graph = graph.split_horizontal(5, 1)
-
-    # Find tops and bottoms
-    tops = ta.find_peaks(df, method=ta.CLASSIC, what=ta.TOPS)       # ta.CLASSIC or ta.MODERN
-    bottoms = ta.find_peaks(df, method=ta.CLASSIC, what=ta.BOTTOMS)
-
-    # Draw candles and volume
-    candle_graph.draw_candles(df, greens=tops, reds=bottoms)
-    volume_graph.draw_bars(df)
+    # Draw candles
+    graph.draw_candles(df)
 
     # Find all trendlines
     support_lines = ta.find_trendlines(df, clearance=2, what=ta.SUPPORTS)
@@ -50,12 +39,12 @@ def main():
             if s_inclination > 0:   # Support line is rising
                 if r_inclination < s_inclination:   # Resist line is not rising as fast as support line
                     print("Found a rising wedge")
-                    candle_graph.draw_lines(df, lines=[s])
-                    candle_graph.draw_lines(df, lines=[r])
+                    graph.draw_lines(df, lines=[s])
+                    graph.draw_lines(df, lines=[r])
 
     # Uncomment these to show all trendlines
-    #candle_graph.draw_lines(df, lines=support_lines)
-    #candle_graph.draw_lines(df, lines=resist_lines)
+    #graph.draw_lines(df, lines=support_lines)
+    #graph.draw_lines(df, lines=resist_lines)
 
     # Show the finished graph
     cv2.imshow('Technical analysis', graph.canvas)
